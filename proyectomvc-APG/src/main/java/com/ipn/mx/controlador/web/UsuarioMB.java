f45a4968b8a25a3b2b0d9217a878c33ed9b41ab6
Atziri <atziripg.99@gmail.com>
2021-01-19 14:27:20 -0600

    usuarios xhtml

--- proyectomvc-APG/src/main/java/com/ipn/mx/controlador/web/UsuarioMB.java
+++ proyectomvc-APG/src/main/java/com/ipn/mx/controlador/web/UsuarioMB.java
@@ -5,10 +5,121 @@
  */
 package com.ipn.mx.controlador.web;
 
+import static com.ipn.mx.controlador.web.BaseBean.ACC_ACTUALIZAR;
+import static com.ipn.mx.controlador.web.BaseBean.ACC_CREAR;
+import com.ipn.mx.modelo.dao.UsuarioDAO;
+import com.ipn.mx.modelo.dto.UsuarioDTO;
+import java.awt.event.ActionEvent;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
 /**
  *
  * @author Atziri Perez
  */
-public class UsuarioMB {
-    
+@ManagedBean(name = "UsuarioMB")
+@SessionScoped
+public class UsuarioMB extends BaseBean implements Serializable {
+
+    private final UsuarioDAO dao = new UsuarioDAO();
+    private UsuarioDTO dto;
+    private List<UsuarioDTO> listaUsuarios;
+
+    /**
+     * Creates a new instance of UsuarioMB
+     */
+    public UsuarioMB() {
+    }
+
+    @PostConstruct
+    public void init() {
+        listaUsuarios = new ArrayList<>();
+        listaUsuarios = dao.readAll();
+    }
+
+    public String prepareAdd() {
+        dto = new UsuarioDTO();
+        setAccion(ACC_CREAR);
+        return "/updateUser?faces-redirect=true";
+    }
+
+    public String prepareUpdate() {
+        setAccion(ACC_ACTUALIZAR);
+        return "/updateUser?faces-redirect=true";
+    }
+
+    public String prepareIndex() {
+        init();
+        return "/listaUsuarios?faces-redirect=true";
+    }
+
+    public String back() {
+        return prepareIndex();
+    }
+
+    public boolean validate() {
+        boolean valido = true;
+        //las validaciones
+        return valido;
+    }
+
+    public String add() {
+        boolean valido = validate();
+        if (valido) {
+            dao.create(dto);
+            return prepareIndex();
+        } else {
+            return prepareAdd();
+        }
+    }
+
+    public String update() {
+        boolean valido = validate();
+        if (valido) {
+            dao.update(dto);
+            return prepareIndex();
+        } else {
+            return prepareUpdate();
+        }
+    }
+
+    public String delete() {
+        dao.delete(dto);
+        return prepareIndex();
+    }
+
+    public void seleccionarUsuario(ActionEvent event) {
+        String claveSel = (String) 
+                FacesContext.getCurrentInstance().
+                        getExternalContext().
+                        getRequestParameterMap().get("claveSel");
+        dto = new UsuarioDTO();
+        dto.getEntidad().setIdUsuario(Integer.parseInt(claveSel));
+        try {
+            dao.read(dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public UsuarioDTO getDto() {
+        return dto;
+    }
+
+    public void setDto(UsuarioDTO dto) {
+        this.dto = dto;
+    }
+
+    public List<UsuarioDTO> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(List<UsuarioDTO> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
 }
